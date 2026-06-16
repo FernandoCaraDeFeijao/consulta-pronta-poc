71463e61e5993b79eb8e2b20ec3c615b1d80a65d
calielian <calielian@proton.me>
2026-06-16 19:51:06 -0300

    feat: added db_generator.sql; logic model and database.php

---
+++ db_generator.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE IF NOT EXISTS consultapronta_poc;
+USE consultapronta_poc;
+
+CREATE TABLE IF NOT EXISTS paciente (
+    id_paciente INTEGER PRIMARY KEY,
+    data_nascimento DATE,
+    altura SMALLINT,
+    peso NUMERIC(5,2),
+    alergias JSON,
+    historico_familiar JSON,
+    doencas JSON,
+    tipo_sanguineo VARCHAR(3)
+
+    CONSTRAINT FK_PACIENTE_id_usuario
+        FOREIGN KEY (id_paciente)
+        REFERENCES usuario(id_usuario);
+);
+
+CREATE TABLE IF NOT EXISTS profissional (
+    id_profissional INTEGER PRIMARY KEY,
+    crm VARCHAR(10),
+    especialidades JSON
+
+    CONSTRAINT FK_PROFISSIONAL_id_usuario
+        FOREIGN KEY (id_profissional)
+        REFERENCES usuario(id_usuario);
+);
+
+CREATE TABLE IF NOT EXISTS sintoma (
+    id_sintoma INTEGER PRIMARY KEY,
+    id_paciente INTEGER,
+    descricao VARCHAR(300),
+    intensidade SMALLINT,
+    data_inicio TIMESTAMP,
+    local VARCHAR(50),
+    status VARCHAR(15)
+
+    CONSTRAINT FK_SINTOMA_id_paciente
+        FOREIGN KEY (id_paciente)
+        REFERENCES paciente(id_paciente);
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id_usuario INTEGER PRIMARY KEY,
+    id_contato INTEGER,
+    nome VARCHAR(100),
+    cpf VARCHAR(11),
+    email VARCHAR(100),
+    senha_hash VARCHAR(255),
+    tipo_usuario CHAR(2),
+    data_cadastro DATE
+
+    CONSTRAINT FK_USUARIO_id_contato
+        FOREIGN KEY (id_contato)
+        REFERENCES contato(id_contato);
+);
+
+CREATE TABLE IF NOT EXISTS autorizacao (
+    id_autorizacao INTEGER PRIMARY KEY,
+    id_paciente INTEGER,
+    id_profissional INTEGER,
+    data_concessao DATE,
+    data_revogacao DATE,
+    status VARCHAR(15)
+
+    CONSTRAINT FK_AUTORIZACAO_id_paciente
+        FOREIGN KEY (id_profissional)
+        REFERENCES profissional(id_profissional);
+    CONSTRAINT FK_AUTORIZACAO_id_paciente
+        FOREIGN KEY (id_paciente)
+        REFERENCES paciente(id_paciente);
+);
+
+CREATE TABLE IF NOT EXISTS prescricao (
+    id_prescricao INTEGER PRIMARY KEY,
+    id_paciente INTEGER,
+    id_profissional INTEGER,
+    medicamentos JSON,
+    orientacoes_uso VARCHAR(,
+    data_emissao DATE,
+    data DATE
+
+    CONSTRAINT FK_PRESCRICAO_id_profissional
+        FOREIGN KEY (id_profissional)
+        REFERENCES profissional(id_profissional);
+    CONSTRAINT FK_PRESCRICAO_id_paciente
+        FOREIGN KEY (id_paciente)
+        REFERENCES paciente(id_paciente);
+);
+
+CREATE TABLE IF NOT EXISTS exame (
+    id_exame INTEGER PRIMARY KEY,
+    id_paciente INTEGER,
+    id_profissional INTEGER,
+    titulo VARCHAR(100),
+    tipo VARCHAR(100),
+    descricao_resultado VARCHAR(100),
+    data_resultado DATE
+
+    CONSTRAINT FK_EXAME_id_paciente
+        FOREIGN KEY (id_paciente)
+        REFERENCES paciente(id_paciente);
+    CONSTRAINT FK_EXAME_id_profissional
+        FOREIGN KEY (id_profissional)
+        REFERENCES profissional(id_profissional);
+);
+
+CREATE TABLE IF NOT EXISTS contato (
+    id_contato INTEGER PRIMARY KEY,
+    tipo VARCHAR(20),
+    valor VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS relatorio (
+    id_relatorio INTEGER PRIMARY KEY,
+    id_paciente INTEGER,
+    id_anotacao INTEGER,
+    data_geracao DATE,
+    periodo_analisado VARCHAR(30),
+    dados_analiticos VARCHAR(200)
+
+    CONSTRAINT FK_RELATORIO_id_paciente
+        FOREIGN KEY (id_paciente)
+        REFERENCES paciente(id_paciente);
+    CONSTRAINT FK_RELATORIO_id_anotacao
+        FOREIGN KEY (id_anotacao)
+        REFERENCES anotacao_clinica(id_anotacao);
+);
+
+CREATE TABLE IF NOT EXISTS anotacao_clinica (
+    id_anotacao INTEGER PRIMARY KEY,
+    id_profissional INTEGER,
+    data_hora TIMESTAMP,
+    texto_evolucao VARCHAR(300),
+    hipotese_diagnostica VARCHAR(100)
+
+    CONSTRAINT FK_ANOTACAO_CLINICA_id_profissional
+        FOREIGN KEY (id_profissional)
+        REFERENCES profissional(id_profissional);
+);
---
+++ src/config/database.php
@@ -0,0 +1,156 @@
+<?php
+
+    function connect_to_database() {
+        $server = "localhost";
+        $user = "root"; 
+        $password = "usbw";
+        $database_name = "consultapronta_poc";
+
+        return mysqli_connect($server, $user, $password, $database_name);
+    }
+
+    function add_new_contact($type, $value) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO contato VALUES
+        ('$type', '$value')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            return mysqli_insert_id($connection); // returns the id of the inserted row
+        } else {
+            // handle failed query
+            return -1;
+        }
+    }
+
+    function add_new_user($connection, $name, $cpf, $email, $password, $user_type, $signup_date) {
+        $sql_command = "INSERT INTO usuario VALUES
+        (null, '$name', '$cpf', '$email', '$password', '$user_type', '$signup_date')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            return mysqli_insert_id($connection); // returns the id of the inserted row
+        } else {
+            // handle failed query
+            return -1;
+        }
+    }
+
+    function add_new_pacient($name, $cpf, $email, $password, $signup_date, $birth_date, $height, $weight, $allergies, $family_history, $diseases, $blood_type) {
+        $connection = connect_to_database();
+        $user_id = add_new_user($connection, $name, $cpf, $email, $password, "paciente", $signup_date);
+
+        $sql_command = "INSERT INTO paciente VALUES
+        ($user_id, '$birth_date', '$height', '$weight', '$allergies', '$family_history', '$diseases', '$blood_type')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+    
+    function add_new_profissional($name, $cpf, $email, $password, $signup_date, $crm, $specialties) {
+        $connection = connect_to_database();
+        $user_id = add_new_user($connection, $name, $cpf, $email, $password, "profissional", $signup_date);
+
+        $sql_command = "INSERT INTO profissional VALUES
+        ($user_id, '$crm', '$specialties')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function add_new_symptom($pacient_id, $description, $intensity, $date_begin, $local, $status) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO sintoma VALUES
+        ($pacient_id, '$description', $intensity, '$date_begin', '$local', '$status')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function add_new_exam($pacient_id, $profissional_id, $title, $type, $result_description, $result_date) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO exame VALUES
+        ($pacient_id, $profissional_id, '$title', '$type', '$result_description', '$result_date')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function add_new_prescription($pacient_id, $profissional_id, $medicines, $use_orietations, $emission_date, $date) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO prescricao VALUES
+        ($pacient_id, $profissional_id, '$medicines', '$use_orietations', '$emission_date', '$date')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function add_new_authorization($pacient_id, $profissional_id, $concession_date, $revocation_date, $status) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO autorizacao VALUES
+        ($pacient_id, $profissional_id, '$concession_date', '$revocation_date', '$status')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function add_new_clinic_note($profissional_id, $datetime, $evolution_text, $diagnostic_hypothesis) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO anotacao_clinica VALUES
+        ($profissional_id, '$datetime', '$evolution_text', '$diagnostic_hypothesis')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function add_new_report($pacient_id, $clinic_note_id, $gen_date, $period_analyzed, $analytical_data) {
+        $connection = connect_to_database();
+
+        $sql_command = "INSERT INTO relatorio VALUES
+        ($pacient_id, $clinic_note_id, '$gen_date', '$period_analyzed', '$analytical_data')";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+    function delete_table_row($table, $row_name, $value) {
+        $connection = connect_to_database();
+
+        $sql_command = "DELETE FROM $table WHERE $row_name = $value";
+
+        if (mysqli_query($connection, $sql_command)) {
+            // handle succesful query
+        } else {
+            // handle failed query
+        }
+    }
+
+?>
