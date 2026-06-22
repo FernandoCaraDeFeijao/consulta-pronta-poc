e9cdf0cacc83fdf611a2178c516eed43a4836a01
calielian <calielian@proton.me>
2026-06-22 19:50:41 -0300

    fix: mysql CREATE TABLE in wrong order

--- db_generator.sql
+++ db_generator.sql
@@ -1,6 +1,27 @@
 CREATE DATABASE IF NOT EXISTS consultapronta_poc;
 USE consultapronta_poc;
 
+CREATE TABLE IF NOT EXISTS contato (
+    id_contato INTEGER PRIMARY KEY,
+    tipo VARCHAR(20),
+    valor VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id_usuario INTEGER PRIMARY KEY,
+    id_contato INTEGER,
+    nome VARCHAR(100),
+    cpf VARCHAR(11),
+    email VARCHAR(100),
+    senha_hash VARCHAR(255),
+    tipo_usuario CHAR(2),
+    data_cadastro DATE,
+
+    CONSTRAINT FK_USUARIO_id_contato
+        FOREIGN KEY (id_contato)
+        REFERENCES contato(id_contato);
+);
+
 CREATE TABLE IF NOT EXISTS paciente (
     id_paciente INTEGER PRIMARY KEY,
     data_nascimento DATE,
@@ -38,21 +59,6 @@ CREATE TABLE IF NOT EXISTS sintoma (
     CONSTRAINT FK_SINTOMA_id_paciente
         FOREIGN KEY (id_paciente)
         REFERENCES paciente(id_paciente);
-);
-
-CREATE TABLE IF NOT EXISTS usuario (
-    id_usuario INTEGER PRIMARY KEY,
-    id_contato INTEGER,
-    nome VARCHAR(100),
-    cpf VARCHAR(11),
-    email VARCHAR(100),
-    senha_hash VARCHAR(255),
-    tipo_usuario CHAR(2),
-    data_cadastro DATE,
-
-    CONSTRAINT FK_USUARIO_id_contato
-        FOREIGN KEY (id_contato)
-        REFERENCES contato(id_contato);
 );
 
 CREATE TABLE IF NOT EXISTS autorizacao (
@@ -105,10 +111,16 @@ CREATE TABLE IF NOT EXISTS exame (
         REFERENCES profissional(id_profissional);
 );
 
-CREATE TABLE IF NOT EXISTS contato (
-    id_contato INTEGER PRIMARY KEY,
-    tipo VARCHAR(20),
-    valor VARCHAR(100)
+CREATE TABLE IF NOT EXISTS anotacao_clinica (
+    id_anotacao INTEGER PRIMARY KEY,
+    id_profissional INTEGER,
+    data_hora TIMESTAMP,
+    texto_evolucao VARCHAR(300),
+    hipotese_diagnostica VARCHAR(100),
+
+    CONSTRAINT FK_ANOTACAO_CLINICA_id_profissional
+        FOREIGN KEY (id_profissional)
+        REFERENCES profissional(id_profissional);
 );
 
 CREATE TABLE IF NOT EXISTS relatorio (
@@ -125,16 +137,4 @@ CREATE TABLE IF NOT EXISTS relatorio (
     CONSTRAINT FK_RELATORIO_id_anotacao
         FOREIGN KEY (id_anotacao)
         REFERENCES anotacao_clinica(id_anotacao);
-);
-
-CREATE TABLE IF NOT EXISTS anotacao_clinica (
-    id_anotacao INTEGER PRIMARY KEY,
-    id_profissional INTEGER,
-    data_hora TIMESTAMP,
-    texto_evolucao VARCHAR(300),
-    hipotese_diagnostica VARCHAR(100),
-
-    CONSTRAINT FK_ANOTACAO_CLINICA_id_profissional
-        FOREIGN KEY (id_profissional)
-        REFERENCES profissional(id_profissional);
 );
