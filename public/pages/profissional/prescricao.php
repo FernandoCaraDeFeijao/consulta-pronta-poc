72d4c6ba2c8e8d3c03935ad91947fc491e428570
Fernando <fernandofkz@hotmail.com>
2026-06-28 16:46:22 -0300

    feat: add "prescrição" page

--- public/pages/profissional/prescricao.php
+++ public/pages/profissional/prescricao.php
@@ -20,6 +20,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/config/global.php";
 			width: 300px;
 			padding: 0;
 			border: none;
+            background-color: #4D4A73;
 		}
 
 		input, select, textarea, button {
@@ -43,33 +44,37 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/config/global.php";
 				filter: brightness(0.8);
 			}
 		}
-		.pesquisa{
-			display: flex;
-			align-items: center;
-			background-color: #F4EFE6;
-			width: 600px;
-			margin: 60px auto 20px;
-			border-radius: 5px;
-			padding: 5px 10px;
-			color: #2B254D;
-		}
-		#inputpesquisa{
-			border: none;
-			background-color: transparent;
-			width: 100%;
-			outline: none;
-		}
-		.containerhead {
-			display: grid;
-			gap: 20px;
-		}
-		.botao-exame {
-			border: 1px solid #2B254D;
-			background-color: transparent;
-			font-size: 14px;
-    		width: 200px;
-			border-radius: 15px;
-		}
+
+        section#periodos{
+            display: grid;
+            grid-template-areas: 
+                "n_comprimidos intervalo_horas"
+                "quant_dias";
+            gap: 1em;
+            height: min(50px, 100px);
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 1fr 1fr;
+        }
+
+        article {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+
+
+        article#n_comprimidos {
+            grid-area: triagem;
+        }
+
+        article#intervalo_horas {
+            grid-area: relatorios;
+        }
+
+        article#quant_dias {
+            grid-area: pacientes;
+}
+
 	</style>
 </head>
 <body>
@@ -79,52 +84,46 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/config/global.php";
 		<header>
 			<h1>Exame</h1>
 		</header>
-		<div class="containerhead">
-			<div class="pesquisa">
-				<span>⌕</span>
-				<input type="text" id="inputpesquisa" placeholder="Buscar exames">
-				<input type="button" class="botao-exame" value="⊕ Solicitar exame">
-			</div>
-		</div>
 		
 		<form method="post">
+            <fieldset>
+                <h1>Preescrever Medicamento</h1>
+            </fieldset>
 			<fieldset>
-				<label for="descricao">O que você está sentindo</label>
+				<label for="descricao">Selecione o paciente</label>
 
-				<input type="text" name="descricao" id="descricao" placeholder="Resuma seus sintomas" required>
-			</fieldset>
-			
-			<fieldset>
-				<p>Qual a intensidade do sintoma?</p>
-
-				<div class="input_wrapper">
-					<label for="intensidade" id="intensidade_atual">5</label>
-					<input type="range" name="intensidade" id="intensidade" min="1" max="10" value="5" required oninput="atualizarIntensidadeAtual(event)">
-				</div>
-			</fieldset>
-			
-			<fieldset>
-				<p>Qando começou?</p>
-
-				<div class="input_wrapper">
-					<input type="date" name="data" id="data" value="<?= date('Y-m-d') ?>">
-					<input type="time" name="hora" id="hora" value="<?= date('H:i') ?>">
-				</div>
-			</fieldset>
-			
-			<fieldset>
-				<label for="local">Em qual local do corpo?</label>
-
-				<select name="local" id="local" required>
-					<option value="" disabled selected hidden>Selecione o local</option>
-					<option value="abdomen">Abdomen</option>
-					<option value="barriga">Barriga</option>
-					<option value="cabeca">Cabeça</option>
-					<option value="costas">Costas</option>
-					<option value="olhos">Olhos</option>
-					<option value="pes">Pés</option>
-					<option value="pescoco">Pescoço</option>
+				<select name="paciente" id="paciente" required>
+					<option value="" disabled selected hidden>Selecione o Paciente</option>
+					<option value="cláudio silva">Cláudio Silva</option>
+					<option value="the robert">The Robert</option>
 				</select>
+			</fieldset>
+			
+            <fieldset>
+				<label for="descricao">Escreva o Nome do medicamente</label>
+
+				<input type="text" name="medicamento" id="medicamento" placeholder="medicamento" required>
+			</fieldset>
+
+            <fieldset>
+				<label for="descricao">Selecione o tipo de consumo</label>
+
+				<select name="consumo" id="consumo" required>
+					<option value="" disabled selected hidden>Tipo de consumo</option>
+					<option value="cláudio silva">Oral</option>
+				</select>
+			</fieldset>
+			
+			
+			<fieldset>
+				<label for="descricao">Preencha</label>
+                
+                <div id="periodos">
+                    <input type="text" name="comprimidos" id="n_comprimidos" placeholder="Num. de comprimidos" required>
+                    <input type="text" name="horas" id="intervalo_horas" placeholder="Intervalo de horas" required>
+                    <input type="text" name="dias" id="quant_dias" placeholder="Quantidade de dias" required>
+                </div>
+				
 			</fieldset>
 
 			<button>Registrar</button>
